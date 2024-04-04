05074744a4169e2068000d9179984496670ae8ee
Kyle678 <73669189+Kyle678@users.noreply.github.com>
2024-04-04 16:29:20 -0500

    created cart and checkout pages
    Created cart page that displays items that were added from the shop page and updates correctly. Added checkout page with a form for all relevant information. Still need to put some finishing touches like rounding off decimal values to 2 decimals and positioning the buttons as well as adding color.

--- assignment02/src/MyApp.js
+++ assignment02/src/MyApp.js
@@ -8,10 +8,47 @@ import items from "./products.json";
 function App(){
 
     const {register, handleSubmit, formState: {errors}} = useForm();
-    const [dataF,setDataF] = useState({});
+    const [dataF,setDataF] = useState([]);
     const [viewer,setViewer] = useState(0);
 
+    const shopView = () => {
+        setViewer(0);
+    }
+
+    const cartView = () => {
+        setViewer(1);
+    }
+
+    const checkoutView = () => {
+        setViewer(2);
+    }
+
     function Browse(){
+
+        const addToCart = (el) => {
+            let alreadyInCart = 0;
+            const updatedDataF = dataF.map(data => {
+                if(data.id === el.id){
+                    alreadyInCart = 1;
+                    return {...data, quantity: data.quantity+1}
+                }else{
+                    return data;
+                }
+            });
+
+            if(alreadyInCart){
+                setDataF(updatedDataF);
+            }else{
+                el.quantity = 1;
+                setDataF([...updatedDataF, el])
+            }
+        }
+
+        const removeFromCart = (el) => {
+            let hardCopy = [...dataF];
+            hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id || cartItem.quantity-- > 1);
+            setDataF(hardCopy);
+        }
 
         const listItems = items.map((el) => (
             <Col key={el.id} xs={12} sm={6} md={4} lg={3} className="mb-4">
@@ -22,8 +59,8 @@ function App(){
                         <Card.Text className="card-text" style={{ height: "10%" }}>{el.category}</Card.Text>
                         <Card.Text className="card-text">${el.price.toFixed(2)}</Card.Text>
                         <div className="mt-auto">
-                            <Button variant="primary">+</Button>
-                            <Button >-</Button>
+                            <Button onClick={() => removeFromCart(el)} variant="light">-</Button>
+                            <Button onClick={() => addToCart(el)} variant="light">+</Button>
                         </div>
                     </Card.Body>
                 </Card>
@@ -37,24 +74,104 @@ function App(){
                         {listItems}
                     </Row>
                 </Container>
+                <Button onClick={() => cartView()} variant="primary">View Cart</Button>
             </div>
         );
     }
 
     function Cart(){
 
+        const listCart = dataF.map((el) => (
+            <Row key={el.id} className="mb-3">
+                <Col xs={12}>
+                    <div className="d-flex align-items-center">
+                        <div>
+                            <img src={el.image} alt={el.title} style={{ maxWidth: "100px", marginRight: "20px" }} />
+                        </div>
+                        <div className="ml-3">
+                            <h5>{el.title}</h5>
+                            <p>Price: ${el.price} | Quantity: {el.quantity}</p>
+                            <p>Total: ${el.price * el.quantity}</p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        ));
+
+        const totalCost = () => {
+            let total = 0;
+            for(let data of dataF) {
+                total += data.price * data.quantity;
+            }
+            return total;
+        }
+
         return(
             <div>
-
+                <Button onClick={() => shopView()} varaint="primary">Back</Button>
+                <div>
+                    <Container>
+                        <h2 style={{marginBottom:"40px"}}>Your Cart</h2>
+                        {listCart}
+                    </Container>
+                </div>
+                <Row className="justify-content-between">
+                    <Col>
+                        <h5 style={{ marginLeft:"120px", marginTop:"20px", marginBottom:"20px"}}>Total Cost: ${totalCost()}</h5>
+                    </Col>
+                    <Col xs="auto">
+                        <Button style={{marginRight:"40px"}} onClick={() => checkoutView()} variant="primary">Proceed to Checkout</Button>
+                    </Col>
+                </Row>
             </div>
         );
     }
 
     function Checkout(){
 
+        const onSubmit = data => {
+            console.log("Data submitting");
+        }
+
         return(
             <div>
-                
+                <Button onClick={() => cartView()} variant="primary">Back</Button>
+                <div>
+                    <form onSubmit={handleSubmit(onSubmit)} className="container mt-5">
+                        <div className="form-group">
+                            <input {...register("fullName", { required: true })} placeholder="Full Name" className="form-control" />
+                            {errors.fullName && <p className="text-danger">Full Name is required.</p>}
+                        </div>
+                        <div className="form-group">
+                          <input {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="Email" className="form-control" />
+                            {errors.email && <p className="text-danger">Email is required.</p>}
+                        </div>
+                        <div className="form-group">
+                            <input {...register("creditCard", { required: true })} placeholder="Credit Card" className="form-control" />
+                            {errors.creditCard && <p className="text-danger">Credit Card is required.</p>}
+                        </div>
+                        <div className="form-group">
+                            <input {...register("address", { required: true })} placeholder="Address" className="form-control" />
+                            {errors.address && <p className="text-danger">Address is required.</p>}
+                        </div>
+                            <div className="form-group">
+                            <input {...register("address2")} placeholder="Address 2" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <input {...register("city", { required: true })} placeholder="City" className="form-control" />
+                            {errors.city && <p className="text-danger">City is required.</p>}
+                        </div>
+                        <div className="form-group">
+                            <input {...register("state", { required: true })} placeholder="State" className="form-control" />
+                            {errors.state && <p className="text-danger">State is required.</p>}
+                        </div>
+                        <div className="form-group">
+                            <input {...register("zip", { required: true })} placeholder="Zip" className="form-control" />
+                            {errors.zip && <p className="text-danger">Zip is required.</p>}
+                        </div>
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         );
     }

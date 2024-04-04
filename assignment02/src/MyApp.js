407d8c97b143ce8c1aba6fc65c220b9e6ef1d88e
Kyle678 <73669189+Kyle678@users.noreply.github.com>
2024-04-04 09:31:35 -0500

    Added buttons to cards
    created buttons to add and remove from cart. Still have to implement logic to add and remove.

--- assignment02/src/MyApp.js
+++ assignment02/src/MyApp.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {useForm} from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
+import "./shop.css";
 import { Container, Card, Row, Col, Button } from 'react-bootstrap';
 import items from "./products.json";
 
@@ -15,11 +16,15 @@ function App(){
         const listItems = items.map((el) => (
             <Col key={el.id} xs={12} sm={6} md={4} lg={3} className="mb-4">
                 <Card style={{ width: "15rem", height: "100%" }}>
-                    <Card.Img className="card-img-top" src={el.image} alt="Card image cap" style={{ height: "200px", objectFit: "cover" }}/>
-                    <Card.Body >
-                        <Card.Title className="card-title">{el.title}</Card.Title>
-                        <Card.Text className="card-text">{el.category}</Card.Text>
-                        <Card.Text className="card-text">{el.price}</Card.Text>
+                    <Card.Img className="card-img-top" src={el.image} alt="Card image cap" style={{ scale: "85%", minHeight: "250px", maxHeight:"250px", width: "auto", objectFit: "cover" }}/>            
+                    <Card.Body className="d-flex flex-column card-content" style={{ height: "100%" }}>
+                        <Card.Title className="card-title" style={{ height: "50%" }}>{el.title}</Card.Title>
+                        <Card.Text className="card-text" style={{ height: "10%" }}>{el.category}</Card.Text>
+                        <Card.Text className="card-text">${el.price.toFixed(2)}</Card.Text>
+                        <div className="mt-auto">
+                            <Button variant="primary">+</Button>
+                            <Button >-</Button>
+                        </div>
                     </Card.Body>
                 </Card>
             </Col>
